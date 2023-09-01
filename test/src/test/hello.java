882f49243d6171ac4aa93d99d9d2b7501a1cdba5
DoHyeonKong <132756641+KKongDo@users.noreply.github.com>
2023-09-01 18:54:15 +0900

    제발

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -2,6 +2,7 @@ package test;
 
 public class hello {
 public static void main(String[] args) {
+	System.out.println("t222");
 	System.out.println("tasd222");
 	System.out.println("ee");
 	System.out.println("12312321");//asdasdfsdf
