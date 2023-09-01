8d879c0266e166f4490432874fd93e21dde14933
syc98syc <116439519+syc98syc@users.noreply.github.com>
2023-09-01 18:19:25 +0900

    이상한가

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -2,7 +2,11 @@ package test;
 
 public class hello {
 public static void main(String[] args) {
+<<<<<<< Updated upstream
 	System.out.println("test222222222222222222");
 	System.out.println("hello!!!!!21312312!!!!");
+=======
+	System.out.println("ささささささ");
+>>>>>>> Stashed changes
 }
 }
