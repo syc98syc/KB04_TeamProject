b78b3ebfc5efad738c26a21e86936198a8771378
DoHyeonKong <132756641+KKongDo@users.noreply.github.com>
2023-09-01 18:46:17 +0900

    123123

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -3,10 +3,7 @@ package test;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("tasd222");
-<<<<<<< Updated upstream
-=======
 	System.out.println("ee");
 	System.out.println("12312321");
->>>>>>> Stashed changes
-}
+	}
 }
