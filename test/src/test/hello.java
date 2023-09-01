d33b6bc245efe721d4fba3af21e086a79e033b4b
DoHyeonKong <132756641+KKongDo@users.noreply.github.com>
2023-09-01 19:07:15 +0900

    Update hello.java

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("t222");
 	System.out.println("tasd222");
 	System.out.println("ee");
-	System.out.println("12312321");//asdasdfsdfasd
+	System.out.println("12312321");//asdasdfsdfasdasdzsd
 	System.out.println("asdasdsa");
 }
 }
