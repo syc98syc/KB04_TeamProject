80047942b3cf422f4f947d8aebe0d41dc629c891
김희주 <67685741+hejoo345@users.noreply.github.com>
2023-09-04 16:21:38 +0900

    test

---
+++ jgig/src/main/java/kb04/ditto/jgig/JgigApplication.java
@@ -0,0 +1,13 @@
+package kb04.ditto.jgig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgigApplication.class, args);
+	}
+
+}
