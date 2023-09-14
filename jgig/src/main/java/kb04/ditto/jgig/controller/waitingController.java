798a5e97b7a1504d5348cf1560e80025fd78880c
김희주 <67685741+hejoo345@users.noreply.github.com>
2023-09-14 16:47:05 +0900

    로그인세션 처리로직 추가

--- jgig/src/main/java/kb04/ditto/jgig/controller/waitingController.java
+++ jgig/src/main/java/kb04/ditto/jgig/controller/waitingController.java
@@ -1,5 +1,7 @@
 package kb04.ditto.jgig.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,25 +33,27 @@ public class waitingController {
 	}
 
 	@PostMapping("/jgig/addWaiting")
-	public String addWaiting(@RequestBody WaitingDto dto, Model model) {
-//		dto.setMem_id("kb0001"); // 아이디 
+	public String addWaiting(@RequestBody WaitingDto dto, Model model, HttpSession session) {
+		String mem_id = (String) session.getAttribute("mem_id");
+		dto.setMem_id(mem_id);
 		waitingMapper.insertWaiting(dto);
 
-//		model.addAttribute("msg","성공 ");
 		return "waiting/findStore";
 	}
 
 	@GetMapping("/jgig/detailWaiting")
-	public String showDetailWaiting(Model model) {
-		String mem_id = "kb0002";
-//		String mem_id = "kb0005";
+	public String showDetailWaiting(Model model, HttpSession session) {
+
+		String mem_id = (String) session.getAttribute("mem_id");
+		if(mem_id == null) {
+			return "redirect:/jgig/login"; 	
+		}
+		
 		WaitingDto dto = waitingMapper.detailWaiting(mem_id);
 		if (dto != null) {
 			model.addAttribute("dto", dto);
-			model.addAttribute("msg", "잇슴니다 ");
 			model.addAttribute("detailYn", "Y");
 		} else {
-			model.addAttribute("msg", "업슴니다 ");
 			model.addAttribute("detailYn", "N");
 		}
 		return "waiting/detailWaiting";
@@ -60,4 +64,44 @@ public class waitingController {
 		waitingMapper.deleteWaiting(dto.getWt_seq());
 		return "redirect:/jgig/detailWaiting";
 	}
+	
+	
+	// 로그인 테스트 
+	@GetMapping("/jgig/login_test")
+	public String loginTest(HttpSession session) {
+		// 로그인 정보를 세션에 저장합니다.
+		session.setAttribute("loggedIn", true);
+		session.setAttribute("mem_id", "kb0002"); // 테스트용 아이디
+		session.setAttribute("mem_nm", "김철수"); // 테스트용 이름
+		session.setAttribute("phone_num", "01054237895"); // 테스트용 폰번호
+		session.setAttribute("ssn", "990101-0000000"); // 테스트용 주민번호
+
+		// 로그인 정보를 저장한 후, 다음 페이지로 리다이렉트합니다.
+		return "redirect:/jgig/findStore"; // 로그인 후의 페이지로 리다이렉트
+	}
+
+	// 테스트용 로그아웃 처리 로직
+	@GetMapping("/jgig/logout_test")
+	public String logoutTest(HttpSession session) {
+		if (session != null) {
+			session.invalidate();
+		}
+
+		return "redirect:/jgig/findStore"; // 로그인 후의 페이지로 리다이렉트
+	}
+
+	// 로그인 체크 함수
+	public String login_check(HttpSession session) {
+		// 세션에서 로그인 정보를 확인
+		Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
+
+		// 로그인 여부를 확인하고, 로그인되지 않은 경우 로그인 페이지로 리다이렉트
+		if (loggedIn == null || !loggedIn) {
+			return "redirect:/jgig/login"; // 로그인 페이지로 리다이렉트합니다.
+		}
+
+		// 로그인된 경우, 세션에서 로그인 아이디를 받아옵니다.
+		String mem_id = (String) session.getAttribute("mem_id");
+		return mem_id;
+	} 
 }
